d4e6c23886f21e9a44f4fb2203c1686234553f72
SefBoonen <1boonen.sef1@gmail.com>
2023-09-02 10:02:17 +0200

    Working when ran with node

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 async function main() {
-    await fetch("https://api.giphy.com/v1/gifs/translate?api_key=WgYyQ9MGC9cnJ0uaM45WGdKN8arhwr00&s=cats", {
-        mode: "cors"
-    })
-    .then(function(response) {
-        console.log(response.json());
-    })
-    .catch(function(err) {
-        console.log(err)
-    });
+    const response = await fetch("https://ch.tetr.io/api/users/slowmodead");
+    console.log(await response.json());
 }
 
 main();
