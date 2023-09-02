008ef306339e9d53381e5492e0762507310946dd
SefBoonen <1boonen.sef1@gmail.com>
2023-09-02 11:29:31 +0200

    Trying out nodejs

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -6,9 +8,13 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
-    res.end("hello world");
+    res.end("hello world1d");
 });
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
+})
+
+fs.appendFile(path.join(__dirname, "test.txt"), "node test", err => {
+    if (err) throw err;
 })
