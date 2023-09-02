77b7296371b0e717edbde6d77e81be2a35df8624
SefBoonen <1boonen.sef1@gmail.com>
2023-09-02 19:25:53 +0200

    Working print in console.

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+// const http = require("http");
+// const fs = require("fs");
+// const path = require("path");
 
-const hostname = "127.0.0.1";
-const port = 3000;
+// const hostname = "127.0.0.1";
+// const port = 3000;
 
-const data = fetch("https://ch.tetr.io/api/users/slowmodead")
-
-console.log(data)
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("data");
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+fetch("https://ch.tetr.io/api/users/slowmodead").then(response => {
+    return response.json();
+}).then(response => {
+    console.log(response)
 })
 
-fs.appendFile(path.join(__dirname, "test.txt"), "node test", err => {
-    if (err) throw err;
-})
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", "text/plain");
+//     res.end("data");
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// })
+
+// fs.appendFile(path.join(__dirname, "test.txt"), "node test", err => {
+//     if (err) throw err;
+// })
