30cbba9df5d213857bf76b27fe72d205178431c7
SefBoonen <1boonen.sef1@gmail.com>
2023-09-07 17:28:53 +0200

    Make backend server front

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,49 @@
-// const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-// const hostname = "127.0.0.1";
-// const port = 3000;
-
-const data = fetch("https://ch.tetr.io/api/users/slowmodead").then(response => {
-    return response.json();
-})
-
-console.log(data.then(e => {
-    fs.appendFile(path.join(__dirname, "tetrio.json"), JSON.stringify(e, null, 4), err => {
-        if (err) throw err;
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, "public", req.url === "/" ? "index.html" : req.url);
+  
+    let extname = path.extname(filePath);
+  
+    let contentType = "text/html";
+  
+    switch(extname) {
+      case ".js":
+        contentType = "text/javascript";
+        break;
+      case ".css":
+        contentType = "text/css";
+      case ".json":
+        contentType = "application/json";
+        break;
+      case ".png":
+        contentType = "image/png";
+        break;
+      case ".jpg":
+        contentType = "image/jpg";
+        break;
+    }
+  
+    fs.readFile(filePath, (err, content) => {
+      if(err) {
+        if(err.code == "ENOENT") {
+          fs.readFile(path.join(__dirname, "public", "404.html"), (err, content) => {
+            res.writeHead(200, { "Content-Type": "text/html"});
+            res.end(content, 'utf8');
+          })
+        } else {
+          res.writeHead(500);
+          res.end(`Server Error: ${err.code}`);
+        }
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html"});
+        res.end(content, 'utf8');
+      }
     })
-}))
-
-// const server = http.createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader("Content-Type", "text/plain");
-//     res.end("data");
-// });
-
-// server.listen(port, hostname, () => {
-//     console.log(`Server running at http://${hostname}:${port}/`);
-// })
+  });
+  
+  server.listen(port, () => {
+    console.log(`Server running on port ${port}/`);
+  });
 
