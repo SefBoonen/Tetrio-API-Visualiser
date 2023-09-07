ef095cd4c27429b97dff9a8a4251fdc2875e048c
SefBoonen <1boonen.sef1@gmail.com>
2023-09-07 17:42:59 +0200

    Working server back end

--- backend/index.js
+++ backend/index.js
@@ -1,50 +1,20 @@
 const fs = require("fs");
 const path = require("path");
-const http = requier('http');
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
 
 const server = http.createServer((req, res) => {
-    let filePath = path.join(__dirname, "public", req.url === "/" ? "index.html" : req.url);
-
-    let extname = path.extname(filePath);
-
-    let contentType = "text/html";
-
-    switch (extname) {
-        case ".js":
-            contentType = "text/javascript";
-            break;
-        case ".css":
-            contentType = "text/css";
-        case ".json":
-            contentType = "application/json";
-            break;
-        case ".png":
-            contentType = "image/png";
-            break;
-        case ".jpg":
-            contentType = "image/jpg";
-            break;
-    }
-
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            if (err.code == "ENOENT") {
-                fs.readFile(path.join(__dirname, "public", "404.html"), (err, content) => {
-                    res.writeHead(200, { "Content-Type": "text/html" });
-                    res.end(content, 'utf8');
-                })
-            } else {
-                res.writeHead(500);
-                res.end(`Server Error: ${err.code}`);
-            }
-        } else {
+    if(req.url === "/") {
+        fs.readFile(path.join(__dirname, "frontend", "index.html"), (err, content) => {
             res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(content, 'utf8');
-        }
-    })
+            res.end(content);
+        })
+    }
 });
 
-server.listen(port, () => {
-    console.log(`Server running on port ${port}/`);
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
 
