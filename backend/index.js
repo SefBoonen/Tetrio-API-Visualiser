1d7c9ca37c6c04761b09beb0a4ab87daea716f07
SefBoonen <1boonen.sef1@gmail.com>
2023-09-08 08:05:16 +0200

    Added libs

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 const http = require('http');
+const express = require('express');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
     if(req.url === "/") {
-        fs.readFile(path.join(__dirname, "frontend", "index.html"), (err, content) => {
+        fs.readFile(path.join(__dirname, "../frontend", "index.html"), (err, content) => {
             res.writeHead(200, { "Content-Type": "text/html" });
             res.end(content);
         })
