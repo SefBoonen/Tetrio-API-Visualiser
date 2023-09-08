1c5c5a560cadf7cc0ce262913e29f2f024fe81dd
SefBoonen <1boonen.sef1@gmail.com>
2023-09-08 08:10:00 +0200

    Updated to express

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,33 @@
 const fs = require("fs");
 const path = require("path");
-const http = require('http');
+// const http = require('http');
 const express = require('express');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = express();
 
-const server = http.createServer((req, res) => {
-    if(req.url === "/") {
-        fs.readFile(path.join(__dirname, "../frontend", "index.html"), (err, content) => {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(content);
-        })
-    }
-});
+app.get("/", (req, res) => {
+    fs.readFile(path.join(__dirname, "../frontend", "index.html"), (err, content) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+    })
+})
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(3000);
+
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     if(req.url === "/") {
+//         fs.readFile(path.join(__dirname, "../frontend", "index.html"), (err, content) => {
+//             res.writeHead(200, { "Content-Type": "text/html" });
+//             res.end(content);
+//         })
+//     }
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
 
