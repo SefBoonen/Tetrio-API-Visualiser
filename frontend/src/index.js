b5e85a20f7d8bdb1dd51f2fc6fa0ee78f538dc5c
SefBoonen <1boonen.sef1@gmail.com>
2023-09-13 09:41:25 +0200

    Formatting

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,40 +8,39 @@ const baseUrl = "http://127.0.0.1:3000/";
 
 button.addEventListener("click", sendToBack)
 
-async function sendToBack(e) {
-    e.preventDefault();
-    const res = await fetch(`${baseUrl}username`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            parcel: input.value
-        })
+async function sendToBack() {
+  await fetch(`${baseUrl}username`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      parcel: input.value
     })
+  })
 
-    const rec = await fetch(`${baseUrl}info`, {
-        method: "GET"
-    });
+  const res = await fetch(`${baseUrl}info`, {
+    method: "GET"
+  });
 
-    const data = await rec.json();
+  const data = await res.json();
 
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          }
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
         }
-      });
+      }
+    }
+  });
 }
