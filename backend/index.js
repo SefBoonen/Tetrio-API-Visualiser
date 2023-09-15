0fc6527b80e907c7663db34d22b5dc03eeba0561
SefBoonen <1boonen.sef1@gmail.com>
2023-09-15 07:52:51 +0200

    Code formatting

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const express = require('express');
+const express = require("express");
 const fs = require("fs");
 
 const app = express();
@@ -19,56 +19,70 @@ app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
 setInterval(fetchRecentStream, interval);
 
 async function fetchRecentStream() {
-    console.log("5 min")
-    recentDataStream = JSON.parse(fs.readFileSync(path.join(__dirname, "..", `RecentUserData${slowmoUserId}.json`), "utf-8", (err, data) => {
-        if (err) {
-            throw err;
-        }
-        return data;
-    }));
+    console.log("5 min");
+    recentDataStream = JSON.parse(
+        fs.readFileSync(
+            path.join(__dirname, "..", `RecentUserData${slowmoUserId}.json`),
+            "utf-8",
+            (err, data) => {
+                if (err) {
+                    throw err;
+                }
+                return data;
+            }
+        )
+    );
 
     let ids = [];
 
-    recentDataStream.map(data => {
-        ids.push(data._id)
-    })
+    recentDataStream.map((data) => {
+        ids.push(data._id);
+    });
 
-    const recentStream = await fetch(`https://ch.tetr.io/api/streams/any_userrecent_${slowmoUserId}`).then(response => {
+    const recentStream = await fetch(
+        `https://ch.tetr.io/api/streams/any_userrecent_${slowmoUserId}`
+    ).then((response) => {
         return response.json();
     });
 
     const filteredData = [];
 
-    recentStream.data.records.map(data => {
+    recentStream.data.records.map((data) => {
         if (data.endcontext.gametype == "40l") {
             filteredData.push(data);
         }
     });
 
-    filteredData.map(data => {
+    filteredData.map((data) => {
         if (!ids.includes(data._id)) {
             recentDataStream.push(data);
         }
     });
 
-    fs.writeFileSync(path.join(__dirname, "..", `RecentUserData${slowmoUserId}.json`), JSON.stringify(recentDataStream, null, 4), (err) => {
-        if (err) {
-            console.log(err);;
+    fs.writeFileSync(
+        path.join(__dirname, "..", `RecentUserData${slowmoUserId}.json`),
+        JSON.stringify(recentDataStream, null, 4),
+        (err) => {
+            if (err) {
+                console.log(err);
+            }
         }
-    });
+    );
 }
 
 app.post("/username", async (req, res) => {
     const { parcel } = req.body;
 
     if (!parcel) {
-        return res.status(400).send({ status: "failed" })
+        return res.status(400).send({ status: "failed" });
     }
 
     //get all 40L
-    const user = await fetch(`https://ch.tetr.io/api/users/${parcel}`).then(response => {
-        return response.json();
-    })
+    const user = await fetch(`https://ch.tetr.io/api/users/${parcel}`).then(
+        (response) => {
+            return response.json();
+        }
+    );
 
     if (!user.success) {
         return res.status(400).send({ status: "user not found" });
@@ -76,29 +90,31 @@ app.post("/username", async (req, res) => {
 
     const userid = user.data.user._id;
 
-    const news = await fetch(`https://ch.tetr.io/api/news/user_${userid}?limit=100`).then(response => {
+    const news = await fetch(
+        `https://ch.tetr.io/api/news/user_${userid}?limit=100`
+    ).then((response) => {
         return response.json();
-    })
+    });
 
     const data = news.data.news;
 
     user40Ldata = [];
-    data.map(e => {
+    data.map((e) => {
         if (e.data.gametype == "40l") {
             user40Ldata.push(e);
         }
-    })
-    res.status(200).send({ status: "received" })
+    });
+    res.status(200).send({ status: "received" });
 });
 
 app.get("/info", (req, res) => {
     res.status(200).json(user40Ldata);
-})
+});
 
 app.get("/runs", (req, res) => {
     res.status(200).json(recentDataStream);
-})
+});
 
 app.listen(3000, "127.0.0.1", () => {
-    console.log(`Server running at http://127.0.0.1:3000`)
+    console.log(`Server running at http://127.0.0.1:3000`);
 });
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import Chart from "chart.js/auto";
 import "chartjs-adapter-date-fns";
-import zoomPlugin from 'chartjs-plugin-zoom';
+import zoomPlugin from "chartjs-plugin-zoom";
 
 Chart.register(zoomPlugin);
 
@@ -13,161 +13,161 @@ const colors = ["red", "green", "blue"];
 let lines = 0;
 
 const chart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: [],
-    datasets: []
-  },
-  options: {
-    scales: {
-      x: {
-        type: "time"
-      },
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
+    type: "line",
+    data: {
+        labels: [],
+        datasets: [],
+    },
+    options: {
+        scales: {
+            x: {
+                type: "time",
+            },
+            y: {
+                beginAtZero: true,
+            },
+        },
+    },
 });
 
 const baseUrl = "http://127.0.0.1:3000/";
 
-button.addEventListener("click", sendToBack)
+button.addEventListener("click", sendToBack);
 
 drawRuns();
 
 async function drawRuns() {
-  const chartRuns = new Chart(ctxRuns, {
-    type: 'line',
-    data: {
-      labels: [],
-      datasets: []
-    },
-    options: {
-      plugins: {
-        zoom: {
-          zoom: {
-            wheel: {
-              enabled: true
+    const chartRuns = new Chart(ctxRuns, {
+        type: "line",
+        data: {
+            labels: [],
+            datasets: [],
+        },
+        options: {
+            plugins: {
+                zoom: {
+                    zoom: {
+                        wheel: {
+                            enabled: true,
+                        },
+                        drag: {
+                            enabled: true,
+                        },
+                    },
+                },
             },
-            drag: {
-              enabled: true
-            }
-          },
+            scales: {
+                x: {
+                    type: "time",
+                },
+                y: {
+                    beginAtZero: true,
+                },
+            },
         },
-      },
-      scales: {
-        x: {
-          type: "time"
-        },
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
-
-  await fetch(`${baseUrl}username`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      parcel: "slowmodead"
-    })
-  })
-
-  const res = await fetch(`${baseUrl}info`, {
-    method: "GET"
-  });
-
-  const data = await res.json();
-
-  const dataset = {
-    data: [],
-    label: `slowmodead`,
-    borderWidth: 2,
-    stepped: "after",
-    borderColor: "blue",
-    pointRadius: 4,
-  };
-
-  data.map(i => {
-    dataset.data.push({
-      x: i.ts,
-      y: i.data.result / 1000,
     });
-  })
 
-  chartRuns.data.datasets.push(dataset)
-  chartRuns.update();
+    await fetch(`${baseUrl}username`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            parcel: "slowmodead",
+        }),
+    });
 
-  const runs = await fetch(`${baseUrl}runs`, {
-    method: "GET"
-  });
+    const res = await fetch(`${baseUrl}info`, {
+        method: "GET",
+    });
 
-  const dataRuns = await runs.json();
-  console.log(dataRuns);
+    const data = await res.json();
 
-  const datasetRuns = {
-    type: "scatter",
-    data: [],
-    label: "Runs",
-    borderColor: "black",
-    pointRadius: 1,
-  };
+    const dataset = {
+        data: [],
+        label: `slowmodead`,
+        borderWidth: 2,
+        stepped: "after",
+        borderColor: "blue",
+        pointRadius: 4,
+    };
 
-  dataRuns.map(data => {
-    datasetRuns.data.push({
-      x: data.ts,
-      y: data.endcontext.finalTime / 1000,
-    })
-  })
+    data.map((i) => {
+        dataset.data.push({
+            x: i.ts,
+            y: i.data.result / 1000,
+        });
+    });
 
-  chartRuns.data.datasets.push(datasetRuns)
-  chartRuns.update();
+    chartRuns.data.datasets.push(dataset);
+    chartRuns.update();
+
+    const runs = await fetch(`${baseUrl}runs`, {
+        method: "GET",
+    });
+
+    const dataRuns = await runs.json();
+    console.log(dataRuns);
+
+    const datasetRuns = {
+        type: "scatter",
+        data: [],
+        label: "Runs",
+        borderColor: "black",
+        pointRadius: 1,
+    };
+
+    dataRuns.map((data) => {
+        datasetRuns.data.push({
+            x: data.ts,
+            y: data.endcontext.finalTime / 1000,
+        });
+    });
+
+    chartRuns.data.datasets.push(datasetRuns);
+    chartRuns.update();
 }
 
 async function sendToBack() {
-  const user = await fetch(`${baseUrl}username`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      parcel: input.value
-    })
-  })
-
-  if (user.status != 200) {
-    alert("user not found");
-    return;
-  }
-
-  const res = await fetch(`${baseUrl}info`, {
-    method: "GET"
-  });
-
-  const data = await res.json();
-
-  const dataset = {
-    data: [],
-    label: `${input.value}`,
-    borderWidth: 2,
-    stepped: "after",
-    borderColor: colors[lines % 3],
-    pointRadius: 4,
-  };
-
-  lines++;
-
-  data.map(i => {
-    dataset.data.push({
-      x: i.ts,
-      y: i.data.result / 1000,
+    const user = await fetch(`${baseUrl}username`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            parcel: input.value,
+        }),
     });
-  })
 
-  chart.data.datasets.push(dataset)
-  chart.update();
+    if (user.status != 200) {
+        alert("user not found");
+        return;
+    }
+
+    const res = await fetch(`${baseUrl}info`, {
+        method: "GET",
+    });
+
+    const data = await res.json();
+
+    const dataset = {
+        data: [],
+        label: `${input.value}`,
+        borderWidth: 2,
+        stepped: "after",
+        borderColor: colors[lines % 3],
+        pointRadius: 4,
+    };
+
+    lines++;
+
+    data.map((i) => {
+        dataset.data.push({
+            x: i.ts,
+            y: i.data.result / 1000,
+        });
+    });
+
+    chart.data.datasets.push(dataset);
+    chart.update();
 }
